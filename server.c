c013ff940b1850f222cea64f727155cd99c238b1
Sarath Chandra Godithi <sarathchandragodithi@gmail.com>
2023-11-13 03:33:43 +0530

    feat: Implement base server functionality

--- server.c
+++ server.c
@@ -1,4 +1,8 @@
+#include <arpa/inet.h>
+#include <asm-generic/socket.h>
 #include <netdb.h>
+#include <netinet/in.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,7 +10,67 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define PORT "8080"
+
 int main(int argc, char *argv[]) {
-  printf("hello world\n");
-  return 0;
+    int server_fd, client_fd, status;
+    struct addrinfo hints, *res, *p;
+    struct sockaddr_storage client;
+    socklen_t client_len;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if ((status = getaddrinfo(NULL, PORT, &hints, &res) != 0)) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    for (p = res; p != NULL; p = p->ai_next) {
+
+        if ((server_fd =
+                 socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("socket error");
+            continue;
+        }
+
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &(int){1},
+                       sizeof(int)) == -1) {
+            close(server_fd);
+            perror("setsockopt error");
+            continue;
+        }
+
+        if (bind(server_fd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(server_fd);
+            perror("bind error");
+            continue;
+        }
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "Something went wrong.\n");
+        exit(1);
+    }
+
+    listen(server_fd, 20);
+
+    client_len = (sizeof(client));
+    client_fd = accept(server_fd, (struct sockaddr *)&client, &client_len);
+
+    printf("accepted\n");
+
+    size_t buf_size = 4096;
+    char buf[buf_size];
+    recv(client_fd, buf, buf_size, 0);
+
+    buf[buf_size] = '\0';
+    printf("%s\n", buf);
+
+    freeaddrinfo(res);
+
+    return 0;
 }
