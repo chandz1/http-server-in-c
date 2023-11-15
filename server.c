09435abb6b6a63b5407ec1a159a62573dd5525c8
Sarath Chandra Godithi <sarathchandragodithi@gmail.com>
2023-11-15 12:00:25 +0530

    feat: Add additional error checking; debug info

--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+#include "response.h"
 #include <arpa/inet.h>
 #include <asm-generic/socket.h>
 #include <netdb.h>
@@ -53,25 +54,41 @@ int main(int argc, char *argv[]) {
         break;
     }
 
+    freeaddrinfo(res);
+
     if (p == NULL) {
         fprintf(stderr, "Something went wrong.\n");
         exit(1);
     }
 
-    listen(server_fd, 20);
+    if (listen(server_fd, 20) == -1) {
+        perror("listen error");
+        exit(1);
+    }
 
     client_len = (sizeof(client));
     client_fd = accept(server_fd, (struct sockaddr *)&client, &client_len);
+    if (client_fd == -1) {
+        perror("accept error");
+        exit(1);
+    }
 
-    printf("accepted\n");
+    char client_ip[INET_ADDRSTRLEN];
+    inet_ntop(client.ss_family,
+              &(((struct sockaddr_in *)((struct sockaddr *)&client))->sin_addr),
+              client_ip, sizeof client_ip);
+
+    printf("[*] Connected to client on IP: %s\n", client_ip);
 
     memset(buf, 0, MAXBUFSIZE);
     recv(client_fd, buf, MAXBUFSIZE - 1, 0);
 
     buf[MAXBUFSIZE - 1] = '\0';
-    printf("%s\n", buf);
 
-    freeaddrinfo(res);
+    request *http_req = parse_request(buf);
+    printf("[*] Recieved %s request.\n", http_req->method);
+    printf("[*] Recieved request for path: %s\n", http_req->filepath);
+    printf("[*] Recieved request for protocol: %s\n", http_req->protocol);
 
     return 0;
 }
