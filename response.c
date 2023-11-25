6a90010a9704d0d147e6ecff1f5ebc3ef2ad92c5
Sarath Chandra Godithi <sarathchandragodithi@gmail.com>
2023-11-25 11:11:02 +0530

    refactor: Rename http_req to req

--- response.c
+++ response.c
@@ -3,23 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Function to parse http RequestQueue
+// Function to parse Request
 Request *parse_request(char *request_str) {
-    Request *http_req = (Request *)malloc(sizeof(Request));
+    Request *req = (Request *)malloc(sizeof(Request));
     char *request_line = NULL;
     while ((request_line = strtok_r(request_str, "\r\n", &request_str)) !=
            NULL) {
         char *save_ptr;
         strtok_r(request_line, " ", &save_ptr);
         if (strncmp(request_line, "GET", 3) == 0) {
-            http_req->method = GET;
+            req->method = GET;
         }
-        http_req->filepath = strtok_r(NULL, " /", &save_ptr);
-        http_req->protocol = strtok_r(NULL, " ", &save_ptr);
+        req->filepath = strtok_r(NULL, " /", &save_ptr);
+        req->protocol = strtok_r(NULL, " ", &save_ptr);
         break;
     }
-    printf("[*] Recieved %d request.\n", http_req->method);
-    printf("[*] Recieved request for path: %s\n", http_req->filepath);
-    printf("[*] Recieved request for protocol: %s\n", http_req->protocol);
-    return http_req;
+    printf("[*] Recieved %d request.\n", req->method);
+    printf("[*] Recieved request for path: %s\n", req->filepath);
+    printf("[*] Recieved request for protocol: %s\n", req->protocol);
+    return req;
 }
